a989aa20506ebfbfd8da91b10fa0350ea85395a4
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-05-12 14:35:46 -0300

    Inicio lista to do

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { data } from './db/data';
 
 function App() {
+  const [todos, setTodos] = React.useState([])
+
+  React.useEffect(() => {
+    setTodos(data);
+  }, [])
+
+  console.log(todos);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Lista de tarefas</h1>
+      <div>
+        {todos.map((todo) => (
+          <div>
+            <div>
+              <p>{todo.text}</p>
+              <p>({todo.category})</p>
+            </div>
+            <div>
+              <button>Completar</button>
+              <button>x</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/db/data.js
@@ -0,0 +1,20 @@
+export const data = [
+    {
+        id: 1,
+        text: "Criar funcionalidade x no sistema",
+        category: "Trabalho",
+        isCompleted: false
+    },
+    {
+        id: 2,
+        text: "Ir para a academia",
+        category: "Pessoal",
+        isCompleted: false,
+    },
+    {
+        id: 3,
+        text: "Estudar React",
+        category: "Estudos",
+        isCompleted: false,
+    }
+]
